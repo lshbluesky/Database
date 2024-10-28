aa00cb8fb6ff3d28860526148db614b00ae601f9
lshbluesky <61459016+lshbluesky@users.noreply.github.com>
2024-10-28 17:01:41 +0900

    Add account_DB.py

---
+++ Backend Project/account_DB.py
@@ -0,0 +1,43 @@
+import pymysql
+import mysql_connection
+import account
+
+def insert_user(payload, Token):
+    connection = db_connect()
+    cur = connection.cursor(pymysql.cursors.DictCursor)
+
+    try:
+        add_student = """
+        INSERT INTO student(s_no, s_id, s_pw, s_name, s_email, s_token, dno)
+        VALUES (%s, %s, %s, %s, %s, %s, %s)
+        """
+        cur.execute(add_student, (payload.univ_id, payload.id, payload.pw, payload.name, payload.email, Token, payload.department))
+        connection.commit()
+        return True
+    except Exception as e:
+        connection.rollback()
+        return False
+    finally:
+        cur.close()
+        connection.close()
+
+def validate_user(id, pw):
+    connection = db_connect()
+    cur = connection.cursor(pymysql.cursors.DictCursor)
+
+    try:
+        # 매개 변수로 받은 ID를 통하여 해당 ID, PW 조회
+        cur.execute("SELECT s_id, s_pw FROM student WHERE id = %s", (id,))
+        row = cur.fetchone()
+
+        # 만약, 해당 ID 가 존재하고, 로그인 정보가 일치하다면 True 반환
+        if row and id == row['s_id'] and pw == row['s_pw']:
+            return True
+        else:
+            return False
+    except Exception as e:
+        print(f"Error during user validation: {e}")
+        return False
+    finally:
+        cur.close()
+        connection.close()
