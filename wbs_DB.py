f4e65e7cce598caa7204e89af23c6bc08e2c1dd1
lshbluesky <61459016+lshbluesky@users.noreply.github.com>
2024-11-08 19:18:51 +0900

    Add wbs_DB.py; 진척도(WBS) 관련 쿼리 및 함수 개발 준비

---
+++ wbs_DB.py
@@ -0,0 +1,13 @@
+"""
+    CodeCraft PMS Project
+    파일명 : wbs_DB.py
+    마지막 수정 날짜 : 2024/11/08
+"""
+
+import pymysql
+from mysql_connection import db_connect
+
+# 진척도(WBS)를 추가하는 함수
+# 진척도(WBS)를 수정하는 함수
+# 진척도(WBS)를 삭제하는 함수
+# 진척도(WBS)를 모두 조회하는 함수
