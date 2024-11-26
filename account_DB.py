097a57a433a376395a4abc3d2e472d03598b8d19
Changhwan Kim <kimch061279@gmail.com>
2024-11-26 16:05:49 +0900

    edit account_DB.py; edit validate_user func

--- account_DB.py
+++ account_DB.py
@@ -35,15 +35,15 @@ def validate_user(id, pw):
     cur = connection.cursor(pymysql.cursors.DictCursor)
 
     try:
-        # 매개 변수로 받은 ID를 통하여 해당 ID, PW 조회
-        cur.execute("SELECT s_id, s_pw FROM student WHERE s_id = %s", (id,))
+        # 매개 변수로 받은 ID를 통하여 해당 ID, PW, s_no 조회
+        cur.execute("SELECT s_id, s_pw, s_no FROM student WHERE s_id = %s", (id,))
         row = cur.fetchone()
 
-        # 만약, 해당 ID 가 존재하고, 로그인 정보가 일치하다면 True 반환
+        # 만약, 해당 ID가 존재하고, 로그인 정보가 일치한다면 s_no 반환
         if row and id == row['s_id'] and pw == row['s_pw']:
-            return True
+            return row['s_no']  # s_no 반환
         else:
-            return False
+            return None  # 로그인 실패 시 None 반환
     except Exception as e:
         print(f"Error [validate_user] : {e}")
         return e
