f296ef3e62484e3d6049754796dba7819928ee53
Changhwan Kim <kimch061279@gmail.com>
2025-01-04 17:36:25 +0900

    edit wbs_DB.py; edit wbs func

--- wbs_DB.py
+++ wbs_DB.py
@@ -1,53 +1,25 @@
 """
     CodeCraft PMS Project
     파일명 : wbs_DB.py
-    마지막 수정 날짜 : 2024/12/05
+    마지막 수정 날짜 : 2025/01/04
 """
 
 import pymysql
 from mysql_connection import db_connect
 
-# 진척도(WBS) 항목 한 개를 추가하는 함수
-# 추가하려는 진척도 항목 한 개의 내용과 프로젝트 번호를 매개 변수로 받는다
-def add_one_wbs(group1, group2, group3, group4, work, output_file, manager, note, ratio, start_date, end_date, group1no, group2no, group3no, group4no, pid):
-    connection = db_connect()
-    cur = connection.cursor(pymysql.cursors.DictCursor)
-
-    try:
-        add_one_progress = """
-        INSERT INTO progress (group1, group2, group3, group4, work, output_file, manager, note, ratio, start_date, end_date, group1no, group2no, group3no, group4no, p_no)
-        VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s)
-        """
-        cur.execute(add_one_progress, (group1, group2, group3, group4, work, output_file, manager, note, ratio, start_date, end_date, group1no, group2no, group3no, group4no, pid))
-        connection.commit()
-        return True
-    except Exception as e:
-        connection.rollback()
-        print(f"Error [add_one_wbs] : {e}")
-        return e
-    finally:
-        cur.close()
-        connection.close()
-
-# 진척도(WBS) 항목 여러 개를 이차원 배열(리스트)로 받아서 한꺼번에 추가(저장)하는 함수
-# 추가하려는 진척도의 항목이 모두 담긴 이차원 배열(리스트) 및 프로젝트 번호를 매개 변수로 받는다
-# add_one_wbs() 함수를 반복문으로 계속 돌리는 것보다 add_multiple_wbs() 함수를 사용하는 것이 DB에 INSERT 하는 속도가 빠르고 효율이 더 좋다
-# wbs_data 이차원 배열(리스트)에 저장되는 값의 예시 :
-# [[group1, group2, group3, group4, work, output_file, manager, note, ratio, start_date, end_date, group1no, group2no, group3no, group4no],
-#  [group1, group2, group3, group4, work, output_file, manager, note, ratio, start_date, end_date, group1no, group2no, group3no, group4no], ...]
+# 진척도(WBS) 항목 여러 개 추가 및 기존 데이터 삭제 후 재삽입
 def add_multiple_wbs(wbs_data, pid):
     connection = db_connect()
     cur = connection.cursor(pymysql.cursors.DictCursor)
-    
-    # 각각의 진척도 항목에 프로젝트 번호 pid 추가
-    wbs_data_with_pid = [item + [pid] for item in wbs_data]
-    
+
     try:
-        add_multiple_wbs = """
-            INSERT INTO progress (group1, group2, group3, group4, work, output_file, manager, note, ratio, start_date, end_date, group1no, group2no, group3no, group4no, p_no)
-            VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s)
+        # 새로운 데이터 추가
+        wbs_data_with_pid = [item + [pid] for item in wbs_data]
+        add_multiple_wbs_query = """
+        INSERT INTO progress (group1, group2, group3, group4, work, output_file, manager, note, ratio, start_date, end_date, group1no, group2no, group3no, group4no, p_no)
+        VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s)
         """
-        cur.executemany(add_multiple_wbs, wbs_data_with_pid)
+        cur.executemany(add_multiple_wbs_query, wbs_data_with_pid)
         connection.commit()
         return True
     except Exception as e:
@@ -58,105 +30,34 @@ def add_multiple_wbs(wbs_data, pid):
         cur.close()
         connection.close()
 
-# 진척도(WBS) 항목 한 개를 수정하는 함수
-# 수정하려는 진척도 항목의 진척도 번호를 매개 변수로 받는다
-def edit_one_wbs(group1, group2, group3, group4, work, output_file, manager, note, ratio, start_date, end_date, group1no, group2no, group3no, group4no, progress_no):
+# 특정 프로젝트의 모든 WBS 항목 조회
+def fetch_all_wbs(pid):
     connection = db_connect()
     cur = connection.cursor(pymysql.cursors.DictCursor)
 
     try:
-        edit_one_progress = """
-        UPDATE progress
-        SET group1 = %s,
-            group2 = %s,
-            group3 = %s,
-            group4 = %s,
-            work = %s,
-            output_file = %s,
-            manager = %s,
-            note = %s,
-            ratio = %s,
-            start_date = %s,
-            end_date = %s,
-            group1no = %s,
-            group2no = %s,
-            group3no = %s,
-            group4no = %s
-        WHERE progress_no = %s
-        """
-        cur.execute(edit_one_progress, (group1, group2, group3, group4, work, output_file, manager, note, ratio, start_date, end_date, group1no, group2no, group3no, group4no, progress_no))
-        connection.commit()
-        return True
+        cur.execute("SELECT * FROM progress WHERE p_no = %s ORDER BY group1no, group2no, group3no, group4no", (pid,))
+        result = cur.fetchall()
+        return result
     except Exception as e:
-        connection.rollback()
-        print(f"Error [edit_one_wbs] : {e}")
+        print(f"Error [fetch_all_wbs] : {e}")
         return e
     finally:
         cur.close()
         connection.close()
 
-# 진척도(WBS) 항목 한 개를 삭제하는 함수
-# 삭제하려는 진척도 항목의 진척도 번호를 매개 변수로 받는다
-def delete_one_wbs(progress_no):
-    connection = db_connect()
-    cur = connection.cursor(pymysql.cursors.DictCursor)
-
-    try:
-        cur.execute("SELECT COUNT(*) AS cnt FROM progress WHERE progress_no = %s", (progress_no,))
-        result = cur.fetchone()
-
-        if result['cnt'] == 0:
-            print(f"Error [delete_one_wbs] : Progress number {progress_no} does not exist.")
-            return False
-        
-        cur.execute("DELETE FROM progress WHERE progress_no = %s", (progress_no,))
-        connection.commit()
-        return True
-    except Exception as e:
-        connection.rollback()
-        print(f"Error [delete_one_wbs] : {e}")
-        return e
-    finally:
-        cur.close()
-        connection.close()
-
-# 진척도(WBS) 항목을 모두 삭제하는 함수
-# 프로젝트 번호를 매개 변수로 받아서 해당 프로젝트에 속하는 모든 전척도 항목을 삭제한다
+# 특정 프로젝트의 모든 WBS 항목 삭제
 def delete_all_wbs(pid):
     connection = db_connect()
     cur = connection.cursor(pymysql.cursors.DictCursor)
 
     try:
-        cur.execute("SELECT COUNT(*) AS cnt FROM progress WHERE p_no = %s", (pid,))
-        result = cur.fetchone()
-
-        if result['cnt'] == 0:
-            print(f"Error [delete_all_wbs] : Progress data does not exist in Project UID {pid}.")
-            return False
-        
         cur.execute("DELETE FROM progress WHERE p_no = %s", (pid,))
         connection.commit()
         return True
     except Exception as e:
         connection.rollback()
         print(f"Error [delete_all_wbs] : {e}")
-        return e
-    finally:
-        cur.close()
-        connection.close()
-
-# 진척도(WBS)를 모두 조회하는 함수
-# 프로젝트 번호를 매개 변수로 받아서 해당 프로젝트에 속하는 모든 전척도 항목을 조회한다
-def fetch_all_wbs(pid):
-    connection = db_connect()
-    cur = connection.cursor(pymysql.cursors.DictCursor)
-
-    try:
-        cur.execute("SELECT * FROM progress WHERE p_no = %s ORDER BY group1no ASC, group2no ASC, group3no ASC, group4no ASC", (pid,))
-        result = cur.fetchall()
-        return result
-    except Exception as e:
-        print(f"Error [fetch_all_wbs] : {e}")
         return e
     finally:
         cur.close()
